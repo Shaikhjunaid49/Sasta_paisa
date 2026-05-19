f070d8270660f5f445cbb846b62c7e5f85302149
shaikh junaid <junaidsk4901@gmail.com>
2026-05-19 14:12:07 +0530

    add Nav

--- Sasta_paisa/src/App.jsx
+++ Sasta_paisa/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <>
+      <Nav />
       <h1 class="text-3xl font-bold underline text-blue-600">
         Tailwind works!
       </h1>
