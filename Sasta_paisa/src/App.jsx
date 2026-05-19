6a51eb6c2128e31196eec93d259304377c030660
shaikh junaid <junaidsk4901@gmail.com>
2026-05-19 14:02:01 +0530

    adding nav

--- Sasta_paisa/src/App.jsx
+++ Sasta_paisa/src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
     <>
-    <h1>start</h1>
+      <h1 class="text-3xl font-bold underline text-blue-600">
+        Tailwind works!
+      </h1>
     </>
-  )
-
+  );
 }
 
-export default App
+export default App;
