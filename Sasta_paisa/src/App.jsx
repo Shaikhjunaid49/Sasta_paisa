e19b94587657396bf81a4e65ca90e4767900e299
shaikh junaid <junaidsk4901@gmail.com>
2026-05-20 11:07:45 +0530

    Resolved merge conflict

--- Sasta_paisa/src/App.jsx
+++ Sasta_paisa/src/App.jsx
@@ -3,12 +3,8 @@ import HeroSection from "./sections/HeroSection";
 import FloatingText from "./components/FloatingText";
 import LoanServices from "./sections/LoanServices";
 import Footer from "./components/Footer";
-<<<<<<< HEAD
-
-
-=======
 import ContactUs from "./components/ContactUs";
->>>>>>> 94e12e0d79d345edff19aae52950a0e5ab3fbc28
+
 function App() {
   return (
     <>
@@ -16,10 +12,10 @@ function App() {
       <HeroSection />
       <FloatingText />
       <LoanServices />
-      <ContactUs/>
+      <ContactUs />
       <Footer />
     </>
   );
 }
 
-export default App;   
+export default App;
