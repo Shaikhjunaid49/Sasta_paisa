838e53b5d49b54571aa4fb7dfd9cfdde54e3dfe0
Mohammad Adeeb <mdadeeb7866@gmail.com>
2026-05-21 11:16:36 +0530

    Resolved merge conflict

--- Sasta_paisa/src/App.jsx
+++ Sasta_paisa/src/App.jsx
@@ -1,37 +1,26 @@
+import React from "react";
 import Nav from "./components/Nav";
+import Card from "./components/LoanCard";
 import HeroSection from "./sections/HeroSection";
 import FloatingText from "./components/FloatingText";
 import LoanServices from "./sections/LoanServices";
 import Footer from "./components/Footer";
 import ContactUs from "./components/ContactUs";
-
 import WhyChooseUs from "./components/WhyChooseUs";
 import Referral from "./components/Referral";
 import Feedback from "./components/Feedback";
 import Partners from "./sections/Partners";
 
-import EmiCalculator from "./components/EmiCalculator";
-import Referral from "./components/Referral";
-
-
 function App() {
   return (
-
     <>
       <Nav />
       <HeroSection />
       <FloatingText />
       <LoanServices />
-      <ContactUs/>
-      <EmiCalculator/>
-<<<<<<< HEAD
+      <ContactUs />
       <WhyChooseUs />
       <Referral />
-      <Feedback />
-      <Partners />
-=======
-      <Referral/>
->>>>>>> 6338a302d9604cbccb0c48a09316850f0e19e128
       <Footer />
     </>
   );
